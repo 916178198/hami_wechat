65d60047d1dec58105d9df5b353094d7734ca530
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-09-26 11:52:49 -0700

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.setGlobalLogConfig({
   file: 'hami_log.txt',
 });
 
+
 function swipeUp() {
   let { height, width } = device;
   let x = width / 2;
@@ -13,6 +14,14 @@ function swipeUp() {
 device.wakeUp();
 auto.waitFor();
 swipeUp();
+
+var image_paths = [];
+
+events.observeToast();
+events.onToast(function(toast) {
+  image_path = toast.getText().split('"')[1];
+  image_paths.push(image_path);
+});
 
 const { chanUrl, barkUrl, stepInterval, quickChecking, checkingTime } =
   hamibot.env
@@ -26,6 +35,14 @@ function clickElement(element) {
   log(click(element.bounds().centerX(), element.bounds().centerY()));
   log(longClick(element.bounds().centerX(), element.bounds().centerY()));
   sleep(500);
+}
+
+// 退回微信主页
+var back_button = id("et").exists() && id("et").findOne().parent();
+while (back_button) {
+  clickElement(back_button);
+  back_button = id("et").exists() && id("et").findOne().parent();
+  print(back_button);
 }
 
 var contacts = text("Contacts").findOne().parent();
@@ -46,6 +63,13 @@ function findWithScroll(target_name) {
 var target_user = findWithScroll('单工');
 clickElement(target_user);
 
+var moments = text("Moments").findOne().parent();
+clickElement(moments);
+
 log('finish');
 
-launchApp('Hamibot')
+// sleep(50000);
+
+log('launch hamibot');
+
+launchApp('Hamibot');
